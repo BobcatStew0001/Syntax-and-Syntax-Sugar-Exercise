51520075358b4f4c690aa5e495da019c39bcb4ec
Zach Brown <bigcountry66789@gmail.com>
2026-01-11 15:41:32 -0600

    Edited the code to complete the exercise. Tested the code multiple times

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 4; //Inferred Typing: The variables are defined as an integer and string
+            string response;
+
+
+            response = answer < 9 ? "less than nine" : "greater than nine"; //ternary operator 
+            Console.WriteLine($" The answer is {response}"); //string interpolation 
+
+            answer = 10; //Changed the value of variable "answer" for testing 
+            response = answer < 9 ? "less than nine" : "greater than nine"; //Used the same ternary operator statement
+            Console.WriteLine($" The answer is {response}");//Same string interpolation 
+            
+            //Output is correct for both test. 
+            
+            
+
         }
     }
 }
